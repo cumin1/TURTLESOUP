e110b90a3f4c8845211163f483aeb3447b908c16
cumin1 <3144941746@qq.com>
2025-06-25 16:49:45 +0800

    reinit: 重新初始化仓库并上传全部文件

---
+++ turtle-soup-backend/src/main/java/com/turtle/TurtleSoupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.turtle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleSoupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurtleSoupBackendApplication.class, args);
+    }
+
+}
